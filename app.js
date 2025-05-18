92b4506bd38f2aeb8b8b713e98396e80713f4a5e
arthurgitsouza <arthurgitsouza@gmail.com>
2025-05-18 18:11:14 -0300

    update

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 alert('Boas Vindas ao Jogo do Número Secreto!');
 
-let numeroSecreto = 10;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 let chute;
 let tentativas = 1;
 
 console.log(numeroSecreto);
 
 while(chute != numeroSecreto){
-    chute = prompt('Digite um número entre 1 e 50:') // a variável chute receberá o input através da func Prompt()
+    chute = prompt('Digite um número entre 1 e 100:') // a variável chute receberá o input através da func Prompt()
 
     if (chute == numeroSecreto) {
-    alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com o total de ${tentativas} tentativas!`);
+        break
     } else {
         if (chute > numeroSecreto){
             alert(`O número ${chute} é maior que o número secreto!`)
@@ -20,3 +20,7 @@ while(chute != numeroSecreto){
         tentativas+=1 // Ou tentativas++ 
     }
 }
+
+let palavraTentativa =  tentativas > 1 ? 'tentativas' : 'tentativa'
+
+alert(`Parabéns, você acertou o número secreto ${numeroSecreto} com o total de ${tentativas} ${palavraTentativa}!`)
